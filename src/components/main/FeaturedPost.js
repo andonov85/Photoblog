77a080d5056db984bea49a8fa198350d33b0d9f1
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-13 20:29:29 +0300

    Update FeaturedPost.js

--- src/components/main/FeaturedPost.js
+++ src/components/main/FeaturedPost.js
@@ -7,7 +7,6 @@ import CardHeader from '@material-ui/core/CardHeader';
 import CardMedia from '@material-ui/core/CardMedia';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
-import Collapse from '@material-ui/core/Collapse';
 import Avatar from '@material-ui/core/Avatar';
 import IconButton from '@material-ui/core/IconButton';
 import Typography from '@material-ui/core/Typography';
@@ -16,6 +15,7 @@ import FavoriteIcon from '@material-ui/icons/Favorite';
 import ShareIcon from '@material-ui/icons/Share';
 import ExpandMoreIcon from '@material-ui/icons/ExpandMore';
 import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Button from '@material-ui/core/Button';
 
 const styles = theme => ({
   card: {
@@ -69,18 +69,20 @@ class FeaturedPost extends React.Component {
               <MoreVertIcon />
             </IconButton>
           }
-          title="Crysis 3"
-          subheader="August 14, 2018"
+          title = "Hands-on with new Fujifilm XF 200mm F2 and XF 8-16mm F2.8"
+          subheader = "Sep 11, 2018 "
         />
+        <a type="button" target="_blank" rel="noopener noreferrer" 
+        href="https://www.dpreview.com/articles/3133419616/hands-on-with-new-fujifilm-xf-200mm-f2-and-xf-8-16mm-f2-8?slide=2">
         <CardMedia
           className={classes.media}
-          image = "./images/reptile.jpg"
-          title="Contemplative Reptile"
+          image = "https://2.img-dpreview.com/files/p/E~TS589x442~articles/3133419616/lens01.jpeg"
+          title = "Hands-on with new Fujifilm XF 200mm F2 and XF 8-16mm F2.8"
         />
+        </a>
         <CardContent>
           <Typography component="p">
-            This impressive paella is a perfect party dish and a fun meal to cook together with your
-            guests. Add 1 cup of frozen peas along with the mussels, if you like.
+            The new XF 200mm F2 R LM OIS WR looks like it will be a seriously impressive lens (the sample we've had our hands on is a pre-production model, which was not cleared for testing).
           </Typography>
         </CardContent>
         <CardActions className={classes.actions} disableActionSpacing>
@@ -101,35 +103,6 @@ class FeaturedPost extends React.Component {
             <ExpandMoreIcon />
           </IconButton>
         </CardActions>
-        <Collapse in={this.state.expanded} timeout="auto" unmountOnExit>
-          <CardContent>
-            <Typography paragraph variant="body2">
-              Method:
-            </Typography>
-            <Typography paragraph>
-              Heat 1/2 cup of the broth in a pot until simmering, add saffron and set aside for 10
-              minutes.
-            </Typography>
-            <Typography paragraph>
-              Heat oil in a (14- to 16-inch) paella pan or a large, deep skillet over medium-high
-              heat. Add chicken, shrimp and chorizo, and cook, stirring occasionally until lightly
-              browned, 6 to 8 minutes. Transfer shrimp to a large plate and set aside, leaving
-              chicken and chorizo in the pan. Add pimentón, bay leaves, garlic, tomatoes, onion,
-              salt and pepper, and cook, stirring often until thickened and fragrant, about 10
-              minutes. Add saffron broth and remaining 4 1/2 cups chicken broth; bring to a boil.
-            </Typography>
-            <Typography paragraph>
-              Add rice and stir very gently to distribute. Top with artichokes and peppers, and cook
-              without stirring, until most of the liquid is absorbed, 15 to 18 minutes. Reduce heat
-              to medium-low, add reserved shrimp and mussels, tucking them down into the rice, and
-              cook again without stirring, until mussels have opened and rice is just tender, 5 to 7
-              minutes more. (Discard any mussels that don’t open.)
-            </Typography>
-            <Typography>
-              Set aside off of the heat to let rest for 10 minutes, and then serve.
-            </Typography>
-          </CardContent>
-        </Collapse>
       </Card>
     );
   }
