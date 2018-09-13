e2a136062db23a8236071ce93a2e88d8a6c8bcfe
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-13 16:26:04 +0300

    Update imageSource.js

--- src/components/gallery/imageSource.js
+++ src/components/gallery/imageSource.js
@@ -9,22 +9,23 @@ firebase.initializeApp(config);
 function imageSource() {
     const db = firebase.firestore();
 
-    let images = [];
+    return new Promise((resolve) => {
+        let images = [];
 
-    db.collection('images').get().then((snapshot) => {
-        snapshot.forEach((doc) => {
-            images.push({
-                id: doc.data().id,
-                name: doc.data().name,
-                url: doc.data().url,
-                thumbFileUrl: doc.data().thumbFileUrl,
-                category: doc.data().category,
-                description: doc.data().description
+        db.collection('images').get().then((snapshot) => {
+            snapshot.forEach((doc) => {
+                images.push({
+                    id: doc.data().id,
+                    name: doc.data().name,
+                    url: doc.data().url,
+                    thumbFileUrl: doc.data().thumbFileUrl,
+                    category: doc.data().category,
+                    description: doc.data().description
+                });
             });
+            resolve(images);
         });
     });
-
-    return images;
 }
 
 export default imageSource;
