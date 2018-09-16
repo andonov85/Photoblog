e466734d44aa558df254f8a17b4220a4a1f15d60
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-16 12:27:17 +0300

    Update Main.js

--- src/components/main/Main.js
+++ src/components/main/Main.js
@@ -5,34 +5,26 @@ import { withStyles } from '@material-ui/core/styles';
 import Grid from '@material-ui/core/Grid';
 
 import MainTitle from './MainTitle';
-import MainFeaturedPost from './FeaturedPost';
+import FeaturedPost from './FeaturedPost';
 
 const styles = theme => ({
-  layoutMain: {
-    marginTop: '1%',
-    marginLeft: '25%',
-    marginRight: '25%',
-    [theme.breakpoints.down('sm')]: {
-      marginLeft: '0%',
-      marginRight: '0%'
-    }
+  root: {
+    flexGrow: 1
   }
 });
 
 function Main(props) {
   const { classes } = props;
   return (
-    <div className={classes.layout}>
-        <Grid container spacing={16} justify="center">
-          <Grid item xs={12} lg={12}>
-            <MainTitle />
-          </Grid>
-          <div className={classes.layoutMain}>
-          <Grid item xs={12} lg={6}>
-            <MainFeaturedPost />
-          </Grid>
-          </div>
+    <div className={classes.root}>
+      <Grid container spacing={16} justify="center">
+        <Grid item xs={12}>
+          <MainTitle />
         </Grid>
+        <Grid item xs={12}>
+          <FeaturedPost />
+        </Grid>
+      </Grid>
     </div>
 )}
 
