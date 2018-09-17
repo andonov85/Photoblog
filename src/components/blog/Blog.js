b2ff4a15feffdf33d81b23c1c7c332284ac0cd3a
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-17 20:41:59 +0300

    Create Blog.js

---
+++ src/components/blog/Blog.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { withStyles } from '@material-ui/core/styles';
+import Grid from '@material-ui/core/Grid';
+
+import Posts from './Posts'
+
+const styles = theme => ({
+  root: {
+    flexGrow: 1
+  },
+  posts: {
+    marginLeft: '50%',
+    [theme.breakpoints.down('sm')]: {
+      marginLeft: '0%',
+    }
+  }
+});
+
+
+class Blog extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      
+    };
+  }
+
+  render() {
+    const {classes} = this.props;
+
+    return (
+      <div className={classes.root}>
+        <Grid container spacing={8}>
+          <Grid item sm={6} xs={12}>
+            <div className={classes.posts}>
+              <Posts />
+            </div>
+          </Grid>
+          <Grid item sm={6} xs={12}>
+
+          </Grid>
+        </Grid>
+      </div>
+    );
+  }
+}
+
+Blog.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Blog);
