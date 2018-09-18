8fed1608a8508e7c38f600e9cb57fcdc193a9df9
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-18 20:43:12 +0300

    Update blogSource.js

--- src/components/blog/blogSource.js
+++ src/components/blog/blogSource.js
@@ -6,16 +6,18 @@ function blogSource() {
     return new Promise((resolve) => {
         let blogPosts = [];
 
-        db.collection('blog').get().then((snapshot) => {
+        db.collection('blog').orderBy('date', 'desc').get().then((snapshot) => {
             snapshot.forEach((doc) => {
                 const post = doc.data();
+                let jsdate = new Date(post.date.toString());
                 blogPosts.push({
+                    homepageUrl: post.homepageUrl,
                     linkUrl: post.linkUrl,
                     avatarUrl: post.avatarUrl,
                     imageUrl: post.imageUrl,
                     title: post.title,
                     content: post.content,
-                    data: post.data
+                    date: jsdate.toDateString()
                 });
             });
             resolve(blogPosts);
