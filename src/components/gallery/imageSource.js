96f937581e47e92de5590922fd0bbc8fd192241e
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-18 20:43:54 +0300

    Update imageSource.js

--- src/components/gallery/imageSource.js
+++ src/components/gallery/imageSource.js
@@ -7,9 +7,6 @@ function imageSource() {
         let images = [];
 
         db.collection('images').get().then((snapshot) => {
-            let num = 0;
-            const ALL_COLS = 4;
-
             snapshot.forEach((doc) => {
                 const image = doc.data();
                 images.push({
@@ -18,15 +15,8 @@ function imageSource() {
                     url: image.url,
                     thumbUrl: image.thumbUrl,
                     category: image.category,
-                    description: image.description,
-                    cols: Math.floor(Math.random() * 2 + 1)
+                    description: image.description
                 });
-                num += images[images.length - 1].cols;
-                if (num > ALL_COLS) {
-                    images[images.length - 1].cols = 1;
-                } else if (num === ALL_COLS) {
-                    num = 0;
-                }
             });
             resolve(images);
         });
