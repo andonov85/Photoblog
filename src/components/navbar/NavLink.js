c438f4531630a3de7af25d45e5a43b3ba079ce85
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-19 19:42:13 +0300

    Update NavLink.js

--- src/components/navbar/NavLink.js
+++ src/components/navbar/NavLink.js
@@ -1,14 +1,32 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import { Route, Link, withRouter } from 'react-router-dom';
 
+import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
 
-const NavLink = ({ to, location, buttonName }) => (
-  <Route path={to} children={({ match }) => (
-      <Link type="button" replace={to === location.pathname} to={to} style={{textDecoration: "none"}}>
-        <Button>{buttonName}</Button>
-      </Link>
-  )}/>
-)
+const styles = theme => ({
+  buttonsNavbar: {
+    '&:hover': {
+      borderBottom: '4px solid rgb(216, 168, 12)'
+    },
+    color: 'white'
+  }
+});
 
-export default withRouter(NavLink);
+const NavLink = ({ classes, to, location, buttonName }) => {
+  return (
+    <Route path={to} children={({ match }) => (
+      <Link type="button" replace={match === location.pathname} to={to} style={{textDecoration: "none"}}>
+        <Button className={classes.buttonsNavbar}>{buttonName}</Button>
+      </Link>
+    )}/>
+  )
+}
+
+NavLink.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+const routerNavLink = withRouter(NavLink);
+
+export default withStyles(styles)(routerNavLink);
