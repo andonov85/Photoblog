f0dea6e69272d6d6de8ca271784c42c7d4a39ce6
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-24 20:10:31 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Route, BrowserRouter, Switch } from "react-router-dom";
+import { Route, BrowserRouter as Router, Switch } from "react-router-dom";
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
 
@@ -9,41 +9,41 @@ import Grid from '@material-ui/core/Grid';
 import Main from './components/main/Main';
 import About from './components/about/About';
 import Gallery from './components/gallery/Gallery';
-import CategoryGallery from './components/gallery/category/CategoryGallery';
+import Category from './components/gallery/Category';
 import Blog from './components/blog/Blog';
 // import Footer from './components/footer/Footer'
 
 const styles = theme => ({
   root: {
     flexGrow: 1,
-  }
+  },
 });
 
 class App extends Component {
   render() {
-    const {classes} = this.props;
-
+    const { classes } = this.props;
     return (
-      <BrowserRouter>
+      <Router>
           <Grid container spacing={0} className={classes.root}>
             <Grid item xs={12}>
               <NavBar />
             </Grid>
             <Grid item xs={12}>
-              <Switch>
-                <Route exact path="/" component={Main}/>
-                <Route path="/main" component={Main}/>
-                <Route path="/about" component={About}/>
-                <Route path="/gallery" component={Gallery}/>
-                <Route path="/gallery/category" component={CategoryGallery}/>
-                <Route path="/blog" component={Blog}/>
-              </Switch>
+                <Switch>
+                  <Route exact path="/" component={Main}/>
+                  <Route path="/main" component={Main}/>
+                  <Route path="/about" component={About}/>
+                  <Route path="/gallery" component={Gallery}/>
+                  <Route path="/category/:category" component={Category}/>
+                  <Route path="/blog" component={Blog}/>
+                  <Route render={() => <div>Not Found</div>} />
+                </Switch>
             </Grid>
             <Grid item xs={12}>
               {/* ToDo <Route path="/footer" component={Footer}/> */}
             </Grid>
           </Grid>
-      </BrowserRouter>
+      </Router>
     );
   }
 }
