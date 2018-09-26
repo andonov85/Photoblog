5ad5ccebfbc8fee087bc8990b925bec7beb0562b
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-26 15:38:01 +0300

    menu effects

--- src/components/navbar/NavLink.js
+++ src/components/navbar/NavLink.js
@@ -4,12 +4,13 @@ import { Route, Link, withRouter } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
+import './NavLink.css';
 
 const styles = theme => ({
   buttons: {
     '&:hover': {
       borderBottom: '2px solid black',
-      backgroundColor: 'transparent'
+      // backgroundColor: 'transparent'
     },
     fontSize: 13,
     borderRadius: 0
@@ -21,7 +22,17 @@ const NavLink = ({ classes, to, location, buttonName }) => {
     <Route path={to} children={({ match }) => (
       <Link type="button" replace={match === location.pathname} to={to} style={{textDecoration: "none"}}>
         {buttonName === 'Gallery' ? 
-        <Button className={classes.buttons}>{buttonName}</Button>
+        <div className={classes.galleryContainer}>
+          <Button className={classes.buttons + ' ' + 'gallery'}>{buttonName}</Button>
+          <div className="subMenu">
+            <div className="line"/>
+            <div className="subMenuButton">{'Flowers'}</div>
+            <div className="subMenuButton">{'Landscapes and seascapes'}</div>
+            <div className="subMenuButton">{'Macro'}</div>
+            <div className="subMenuButton">{'Black and white'}</div>
+            <div className="subMenuButton">{'Other'}</div>
+          </div>
+        </div>
       :
         <Button className={classes.buttons}>{buttonName}</Button>
       }
