abf4d8d17eed1ef407573ed515d39e4cf45ccd67
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-29 20:49:41 +0300

    Update About.js

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -2,20 +2,32 @@ import React from 'react';
 
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import { Typography } from '@material-ui/core';
 
 // import Grid from '@material-ui/core/Grid';
 
-const styles = {
-	root: {
-
+const styles = (theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+  },
+	paper: {
+    width: 500,
+    height: 800,
+    backgroundColor: '#ebfaf9'
 	}
-};
+});
 
 function About(props) {
-	// const { classes } = props;
+  const { classes } = props;
+  
   return (
-    <div>
-      <Paper>About me</Paper>
+    <div className={classes.root}>
+      <Paper className={classes.paper}>
+        <Typography align='center'>
+          About author
+        </Typography>
+      </Paper>
     </div>
   );
 }
