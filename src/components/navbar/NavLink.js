f6899b7d95aec6b1a19b6212b42f875844f81e6f
andonov85 <36325155+andonov85@users.noreply.github.com>
2018-09-29 20:49:03 +0300

    Update NavLink.js

--- src/components/navbar/NavLink.js
+++ src/components/navbar/NavLink.js
@@ -4,68 +4,29 @@ import { Route, Link, withRouter } from 'react-router-dom';
 
 import { withStyles } from '@material-ui/core/styles';
 import Button from '@material-ui/core/Button';
-import './NavLink.css';
 
-const categories = ['Flowers', 'Landscapes and seascapes', 'Macro', 'Black and white', 'Other'];
 const styles = theme => ({
   buttons: {
-    '&:hover': {
-      borderBottom: '2px solid black',
-      // backgroundColor: 'transparent'
+    [theme.breakpoints.down('sm')]: {
+      color: 'white',
     },
     fontSize: 13,
+    fontFamily: 'Pompiere, cursive',
     borderRadius: 0
-  },
-  galleryContainer: {
-
   }
 });
 
 class NavLink extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      showSubmenu: false
-    };
-    this.handleOnclick = this.handleOnclick.bind(this);
-  }
-
-  handleOnclick(e) {
-    this.setState({
-      showSubmenu: !this.state.showSubmenu
-    });
-  }
-
-  componentDidUpdate() {
-    console.log(this.state.showSubmenu);
-  }
-
   render() {
-  const { classes, to, location, buttonName } = this.props;
-  const { showSubmenu } = this.state;
-  return (
-    <Route path={to} children={({ match }) => 
-      buttonName === 'Gallery'
-      ? 
-        (<div className={classes.galleryContainer} onClick={this.handleOnclick}>
-          {/* <Link type="button" replace={match === location.pathname} to={to} style={{textDecoration: "none"}}> */}
-            <Button className={classes.buttons + ' ' + 'galleryBtn'}>{buttonName}</Button>
-          {/* </Link> */}
-          <div className="subMenu" style={{display: showSubmenu ? 'block' : 'none'}}>
-            <div className="line"/>
-            {categories.map((category) =>
-              (<Link to={`/category/${category.toLowerCase()}`} key={category} style={{textDecoration: "none"}}>
-                <div className="subMenuButton">{category}</div>
-              </Link>)
-            )}
-          </div>
-        </div>)
-      :
-      (<Link type="button" replace={match === location.pathname} to={to} style={{textDecoration: "none"}}>
-        <Button className={classes.buttons}>{buttonName}</Button>
-      </Link>)
-    }/>
-  )}
+    const { classes, to, location, buttonName } = this.props;
+    return (
+      <Route path={to} children={({ match }) => 
+        <Link type="button" replace={match === location.pathname} to={to} style={{textDecoration: "none"}}>
+          <Button className={classes.buttons}>{buttonName}</Button>
+        </Link>
+      }/>
+    )
+  }
 }
 
 NavLink.propTypes = {
